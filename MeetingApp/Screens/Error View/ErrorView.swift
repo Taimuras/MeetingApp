56571c3e17311c4709fbc327e34a65d006c8f3c2
Timur <avelonadreamer@gmail.com>
2023-01-16 10:23:16 +0600

    Added Async Await to Data Store

---
+++ MeetingApp/Screens/Error View/ErrorView.swift
@@ -0,0 +1,55 @@
+//
+//  ErrorView.swift
+//  MeetingApp
+//
+//  Created by Timur on 16/1/23.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        NavigationView{
+            VStack {
+                Text("Hello, World!")
+                    .font(.title)
+                    .padding(.bottom)
+                Text(errorWrapper.error.localizedDescription)
+                    .font(.headline)
+                Text(errorWrapper.guidance)
+                    .font(.caption)
+                    .padding(.top)
+                Spacer()
+            }
+            .padding()
+            .background(.ultraThinMaterial)
+            .cornerRadius(16)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Dismiss") {
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired,
+                     guidance: "You can safely ignore this error.")
+    }
+    
+    static var previews: some View {
+        ErrorView(errorWrapper: wrapper)
+    }
+}
